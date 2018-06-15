b444ef93b4c12c7263d71d9ba2ffc27cb53f8010
Cristiano Pacheco <chris.spb25@gmail.com>
2018-06-15 15:44:05 -0300

    added structure

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 
 import AppRoutes from './app-routes'
+import SideBar from './layout/side-bar'
+import Menu from './layout/menu'
 
 class App extends Component {
   render () {
     return (
-      <div>
-        <AppRoutes />
+      <div className='content'>
+        <Menu />
+        <div className='container'>
+          <div className='main-container'>
+            <SideBar />
+            <AppRoutes />
+          </div>
+        </div>
       </div>
     )
   }
---
+++ src/components/layout/side-bar/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const SideBar = () => (
+  <div className='menu-sidebar'>
+    <h2 className='title'>Documentação</h2>
+    <div className='menu-content'>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default SideBar
